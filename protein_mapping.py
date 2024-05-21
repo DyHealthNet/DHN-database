499aca0df6379fb07dd2b95901239e135f4debba
Leo-Sprengel <leo.sprengel@campus.lmu.de>
2024-05-21 15:37:07 +0200

    fetching proteins from neddrex works, next step is mapping

--- protein_mapping.py
+++ protein_mapping.py
@@ -3,16 +3,54 @@ import json
 import pandas as pd
 import networkx as nx
 import urllib.request
+import requests
 
 def read_proteinID_chris(proteinID_path: str) :
     """
     reads Protein IDs from Chris dataset
     """
     df = pd.read_csv(proteinID_path, sep='\t')
-    print(f"Found {len(df)} proteinIDs with {len(df['protein_id'].unique())} unique ids")
-    return df
+   # print(f"Found {len(df)} proteinIDs with {len(df['UniProt'].unique())} unique ids")
+    return df['UniProt'].unique()
 
-read_proteinID_chris("../data/DyHealthNet/chris_summary_data/proteins/CHRIS_somalogic_descriptive_statistic.txt")
+def get_proteinID_neddrex(proteinID: str):
+    """
+    fetches data for proteinID from neddrex
+    https://api.nedrex.net/relations/get_drugs_targetting_proteins
+    https://api.nedrex.net/relations/get_encoded_proteins
+    https://api.nedrex.net/get_by_id/protein?q=uniprot.Q9UBT6
+    :param proteinID:
+    :return:
+    """
+    url = f'https://api.nedrex.net/get_by_id/protein?q=uniprot.' + proteinID
+    response = requests.get(url)
+    try:
+        data = response.json()
+    except:
+        return None
+    return data
+
+
+
+
+
+
+proteinIDs_CHRIS = read_proteinID_chris("../data/DyHealthNet/chris_summary_data/proteins/CHRIS_somalogic_descriptive_statistic.txt")
+#fetch_gene_names("x0so0034")
+#print(get_proteinID_neddrex("P43320"))
+
+
+for proteinID in proteinIDs_CHRIS:
+    #print(proteinID)
+    print(get_proteinID_neddrex(proteinID))
+
+
+
+
+
+
+
+
 """
 "protein_encoded_by",
 "protein_has_signature",
