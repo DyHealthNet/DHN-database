55f35c548ee1b34050bef2e9be33c1ac9739ad4c
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-05-21 15:08:36 +0200

    consistent naming

--- models.py
+++ models.py
@@ -8,34 +8,34 @@ Base = declarative_base()
 
 class Gene(Base):
     __tablename__ = 'genes'
-    entrezid = Column(String, primary_key=True)
+    entrez_id = Column(String, primary_key=True)
 
 
 class Disorder(Base):
     __tablename__ = 'disorders'
-    mondoid = Column(String, primary_key=True)
+    mondo_id = Column(String, primary_key=True)
     snomed_id = Column(String)
 
 
 class Phenotype(Base):
     __tablename__ = 'phenotypes'
-    hpoid = Column(String, primary_key=True)
+    hpo_id = Column(String, primary_key=True)
     snomed_id = Column(String, nullable=True)
     omim_id = Column(String)
     orpha_id = Column(String)
 
 
 class GeneAssocDisorder(Base):
-    __tablename__ = 'gene_assoc_disorders'
+    __tablename__ = 'gene_associates_disorders'
     id = Column(Integer, primary_key=True)
-    entrezid = Column(String, ForeignKey('genes.entrezid'))
-    mondoid = Column(String, ForeignKey('disorders.mondoid'))
+    entrez_id = Column(String, ForeignKey('genes.entrezid'))
+    mondo_id = Column(String, ForeignKey('disorders.mondoid'))
     edge_source = Column(String)
 
 
 class GeneAssocPhenotype(Base):
-    __tablename__ = 'gene_assoc_phenotypes'
+    __tablename__ = 'gene_associates_phenotypes'
     id = Column(Integer, primary_key=True)
-    entrezid = Column(String, ForeignKey('genes.entrezid'))
-    hpoid = Column(String, ForeignKey('phenotypes.hpoid'))
+    entrez_id = Column(String, ForeignKey('genes.entrezid'))
+    hpo_id = Column(String, ForeignKey('phenotypes.hpoid'))
 
