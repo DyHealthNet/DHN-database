f707f459714f755a99612f7d8f15b99725988cf6
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-05-31 13:57:47 +0200

    update models

--- models.py
+++ models.py
@@ -9,20 +9,43 @@ Base = declarative_base()
 class Gene(Base):
     __tablename__ = 'genes'
     entrez_id = Column(String, primary_key=True)
+    display_name = Column(String)
+    description = Column(String)
+    synonyms = Column(String)
+    chromosome = Column(String)
 
 
 class Disorder(Base):
     __tablename__ = 'disorders'
     mondo_id = Column(String, primary_key=True)
-    snomed_id = Column(String)
+    description = Column(String)
+    xrefs = Column(String)  # take from nedrex
 
 
 class Phenotype(Base):
     __tablename__ = 'phenotypes'
     hpo_id = Column(String, primary_key=True)
     snomed_id = Column(String, nullable=True)
-    omim_id = Column(String)
-    orpha_id = Column(String)
+    description = Column(String)
+    xrefs = Column(String)
+    synonyms = Column(String)
+
+
+class Protein(Base):
+    __tablename__ = 'proteins'
+    uniprot_id = Column(String, primary_key=True)
+    sequence = Column(String)
+    gene_entrez_id = Column(String, ForeignKey('genes.entrez_id'))
+    description = Column(String)
+
+
+class Metabolite(Base):
+    __tablename__ = 'metabolites'
+    hmdb_id = Column(String, primary_key=True)
+    display_name = Column(String)
+    description = Column(String)
+    synonyms = Column(String)
+    xrefs = Column(String)
 
 
 class GeneAssocDisorder(Base):
@@ -41,9 +64,24 @@ class GeneAssocPhenotype(Base):
     edge_source = Column(String)
 
 
-class Protein(Base):
-    __tablename__ = 'proteins'
-    uniprot_id = Column(String, primary_key=True)
-    #entrez_id = Column(String, ForeignKey('genes.entrez_id'))
+class ProteinAssocProtein(Base):
+    __tablename__ = 'protein_associates_proteins'
+    id = Column(Integer, primary_key=True)
+    uniprot_id_source = Column(String, ForeignKey('proteins.uniprot_id'))
+    uniprot_id_target = Column(String, ForeignKey('proteins.uniprot_id'))
 
 
+class ProteinAssocMetabolite(Base):
+    __tablename__ = 'protein_associates_metabolites'
+    id = Column(Integer, primary_key=True)
+    uniprot_id = Column(String, ForeignKey('proteins.uniprot_id'))
+    hmdb_id = Column(String, ForeignKey('metabolites.hmdb_id'))
+    edge_source = Column(String)
+
+
+class MetaboliteAssocDisorder(Base):
+    __tablename__ = 'metabolite_associates_disorders'
+    id = Column(Integer, primary_key=True)
+    hmdb_id = Column(String, ForeignKey('metabolites.hmdb_id'))
+    mondo_id = Column(String, ForeignKey('disorders.mondo_id'))
+    edge_source = Column(String)
