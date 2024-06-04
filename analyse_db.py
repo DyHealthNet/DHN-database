e1b4dbadb050252d12dd3565a44d574e731c89fd
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-06-04 10:37:27 +0200

    database numbers

--- analyse_db.py
+++ analyse_db.py
@@ -1,10 +1,7 @@
 #%%
 from sqlalchemy.orm import sessionmaker
 from setup_db import engine
-from models import Gene, Protein, Phenotype, Disorder
-from query_nedrex import get_needed_snomed_ids
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
+from models import *
 
 #%%
 Session = sessionmaker(bind=engine)
@@ -20,24 +17,11 @@ disorders = session.query(Disorder).count()
 f"Genes: {genes}, Proteins: {proteins}, Disorders: {disorders}, Phenotypes: {phenotypes}"
 
 #%%
-# get the overlap between phenotypes and disorders
-phenotype_overlap = session.query(Phenotype, Disorder).filter(Phenotype.snomed_id == Disorder.snomed_id).all()
-for phenotype, disorder in phenotype_overlap:
-    print(f"Phenotype: {phenotype.hpo_id}, Disorder: {disorder.mondo_id}")
-    print(f"Phenotype: {phenotype.snomed_id}, Disorder: {disorder.snomed_id}")
+# get the edges between each table
+gene_disorder_edges = session.query(GeneAssocDisorder).count()
+disorder_phenotype_edges = session.query(DisorderAssocPhenotype).count()
+protein_assoc_protein_edges = session.query(ProteinAssocProtein).count()
+protein_assoc_gene_edges = session.query(Protein).filter(Protein.gene_entrez_id is not None).count()
 
-#%%
-print(f"Phenotypes that are also considered disorders: {len(phenotype_overlap)}")
-
-#%%
-data_path = '../data/DyHealthNet/chris_summary_data/phenotypes/pheno_meta_all.tsv'
-needed_snomed = get_needed_snomed_ids(data_path)
-
-#%%
-# create venn diagram of phenotypes that are also disorders
-venn2(subsets=(disorders - len(phenotype_overlap), phenotypes - len(phenotype_overlap), len(phenotype_overlap)),
-      set_labels=('Disorders', 'Phenotypes'))
-plt.savefig('phenotype_disorder_overlap_venn.png')
-plt.show()
-
-#%%
+(f"Gene-Disorder edges: {gene_disorder_edges}, Disorder-Phenotype edges: {disorder_phenotype_edges}, "
+ f"Protein-Protein edges: {protein_assoc_protein_edges}, Protein-Gene edges: {protein_assoc_gene_edges}")
