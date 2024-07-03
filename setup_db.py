b72df38550f761b10ee3f744ef2af3a04cd26e9a
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-07-03 11:45:11 +0200

    add all data

--- setup_db.py
+++ setup_db.py
@@ -499,7 +499,7 @@ def testingSetup(session):
 
 if __name__ == '__main__':
     # Variant_affects_gene.__table__.drop(engine, checkfirst=True)
-    # Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)
     # cohort study
     observations = "CHRIS"
     # Define a session
@@ -515,20 +515,20 @@ if __name__ == '__main__':
     metabo_data_path = '../data/DyHealthNet/chris_summary_data/metabolites/CHRIS_biocristes7500SumStats.txt'
     edges_path = '../data/scores.csv'
     # testingSetup(session)
-    # add_disorder_data(session, pheno_data_path, obs_source=observations)
-    # add_phenotype_data(session, pheno_data_path, obs_source=observations)
-    #
-    # add_protein_data(session, protein_data_path, obs_source=observations)
-    # add_metabolite_data(session, metabo_data_path, obs_source=observations)
+    add_disorder_data(session, pheno_data_path, obs_source=observations)
+    add_phenotype_data(session, pheno_data_path, obs_source=observations)
 
-    # gene_ids = {str(row[0]) for row in session.query(Gene.entrez_id).all()}
-    # add_genomic_variants(session, gene_ids, observation_source='external')
+    add_protein_data(session, protein_data_path, obs_source=observations)
+    add_metabolite_data(session, metabo_data_path, obs_source=observations)
+
+    gene_ids = {str(row[0]) for row in session.query(Gene.entrez_id).all()}
+    add_genomic_variants(session, gene_ids, observation_source='external')
 
     # add the edges calculated from the available data
     add_calculated_edges(session, edges_path, pheno_data_path, protein_data_path, metabo_data_path)
 
     # second pass for phenotypes
-    # add_phenotype_data(session, pheno_data_path, obs_source='external')
+    add_phenotype_data(session, pheno_data_path, obs_source='external')
     countEntries(session, metadata)
 
 
